08d0ac5edabd35b341901d95addf8171d9f341cc
FoofyO <wan345sww344@gmail.com>
2022-09-24 19:42:34 +0400

    UI

--- Winlocker/MainWindow.xaml.cs
+++ Winlocker/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Winlocker
 {
@@ -20,6 +8,28 @@ namespace Winlocker
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Unlock(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnBackspace(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnClick(object sender, EventArgs e)
+        {
+        }
+
+        private void OnCapsLock(object sender, EventArgs e)
+        {
         }
     }
 }
